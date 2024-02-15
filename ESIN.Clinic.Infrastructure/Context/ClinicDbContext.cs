c20b4902722399d28ba68ff04cf7d84229c3e26c
Leandro Gabriel <leandro@bigiogabriel.com>
2024-02-15 18:25:10 +0000

    Add new context

---
+++ ESIN.Clinic.Infrastructure/Context/ClinicDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ESIN.Clinic.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESIN.Clinic.Infrastructure.Context;
+
+public class ClinicDbContext : DbContext
+{
+    public ClinicDbContext(DbContextOptions<ClinicDbContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<DeviceCategory> DeviceCategories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClinicDbContext).Assembly);
+    }
+}
