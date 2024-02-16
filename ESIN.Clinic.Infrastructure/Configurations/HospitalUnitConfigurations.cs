b85c7b16275745e170db3eeff5a3e34775aeb313
Leandro Gabriel <leandro@bigiogabriel.com>
2024-02-16 10:47:05 +0000

    Add HospitalUnit configurations

---
+++ ESIN.Clinic.Infrastructure/Configurations/HospitalUnitConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using ESIN.Clinic.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ESIN.Clinic.Infrastructure.Configurations;
+
+public class HospitalUnitConfigurations : IEntityTypeConfiguration<HospitalUnit>
+{
+    public void Configure(EntityTypeBuilder<HospitalUnit> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).IsRequired();
+        builder.Property(x => x.Room).HasMaxLength(50);
+    }
+}
