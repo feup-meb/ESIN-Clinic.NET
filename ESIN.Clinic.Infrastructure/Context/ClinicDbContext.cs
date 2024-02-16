c8f87955a5d8fbdcb851ad96adca2bb8377da43c
Leandro Gabriel <leandro@bigiogabriel.com>
2024-02-16 08:53:45 +0000

    Add remaining DbSets as comment

--- ESIN.Clinic.Infrastructure/Context/ClinicDbContext.cs
+++ ESIN.Clinic.Infrastructure/Context/ClinicDbContext.cs
@@ -10,7 +10,14 @@ public class ClinicDbContext : DbContext
     {
     }
 
+    // public DbSet<Intervention> Interventions { get; set; }
+    // public DbSet<HospitalUnit> HospitalUnits { get; set; }
+    // public DbSet<Equipment> Equipments { get; set; }
     public DbSet<Category> Categories { get; set; }
+    // public DbSet<Manufacturer> Manufacturers { get; set; }
+    // public DbSet<EquipmentAccess> EquipmentAccesses { get; set; }
+    // public DbSet<UserType> UserTypes { get; set; }
+    // public DbSet<Employee> Employees { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
