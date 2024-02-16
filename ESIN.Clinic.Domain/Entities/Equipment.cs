921a04f73fb7eb5612941f3c66a68b9e0760e821
Leandro Gabriel <leandro@bigiogabriel.com>
2024-02-16 10:57:16 +0000

    Add Equipment configurations

--- ESIN.Clinic.Domain/Entities/Equipment.cs
+++ ESIN.Clinic.Domain/Entities/Equipment.cs
@@ -3,9 +3,9 @@
 public class Equipment
 {
     public int Id { get; set; }
-    public string SerialNumber { get; set; }
-    public string Name { get; set; }
-    public string Model { get; set; }
+    public required string SerialNumber { get; set; }
+    public required string Name { get; set; }
+    public required string Model { get; set; }
     public string? Description { get; set; }
     public DateOnly AcquisitionDate { get; set; }
     public DateOnly WarrantyDate { get; set; }
@@ -13,6 +13,9 @@ public class Equipment
     public bool IsActive { get; set; }
     
     public int ManufacturerId { get; set; }
-    public int DeviceCategoryId { get; set; }
+    public required Manufacturer Manufacturer { get; set; }
+    public int CategoryId { get; set; }
+    public required Category Category { get; set; }
     public int HospitalUnitId { get; set; }
+    public required HospitalUnit HospitalUnit { get; set; }
 }
