f5a8402fa58b57dc86fcbb9c1ded175334f7373d
Leandro Gabriel <leandro@bigiogabriel.com>
2024-02-16 10:59:46 +0000

    Add EquipmentAccess configurations

--- ESIN.Clinic.Domain/Entities/EquipmentAccess.cs
+++ ESIN.Clinic.Domain/Entities/EquipmentAccess.cs
@@ -5,5 +5,7 @@ public class EquipmentAccess
     public int Id { get; set; }
 
     public int EmployeeId { get; set; }
-    public int DeviceId { get; set; }
+    public required Employee Employee { get; set; }
+    public int EquipmentId { get; set; }
+    public required Equipment Equipment { get; set; }
 }
---
+++ ESIN.Clinic.Infrastructure/Configurations/EquipmentAccessConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using ESIN.Clinic.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ESIN.Clinic.Infrastructure.Configurations;
+
+public class EquipmentAccessConfigurations : IEntityTypeConfiguration<EquipmentAccess>
+{
+    public void Configure(EntityTypeBuilder<EquipmentAccess> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.HasOne(x => x.Equipment);
+        builder.HasOne(x => x.Employee);
+    }
+}
