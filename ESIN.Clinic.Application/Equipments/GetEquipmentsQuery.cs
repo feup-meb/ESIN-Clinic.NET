290b20c245e128d61f429e508ac91bd4ed196a33
Leandro Gabriel <leandro@bigiogabriel.com>
2024-02-20 15:42:33 +0000

    Add fow for equipments list

---
+++ ESIN.Clinic.Application/Equipments/GetEquipmentsQuery.cs
@@ -0,0 +1,17 @@
+﻿using ESIN.Clinic.Domain.Abstractions;
+using ESIN.Clinic.Domain.Entities;
+
+namespace ESIN.Clinic.Application.Equipments;
+
+public class GetEquipmentsQuery(IEquipmentRepository equipmentRepository)
+{
+    public async Task<List<Equipment>> GetEquipmentsAsync()
+    {
+        var equipments = await equipmentRepository.GetEquipments();
+
+        if (!equipments.Any())
+            throw new Exception("No equipments found.");
+
+        return equipments.ToList();
+    }
+}
