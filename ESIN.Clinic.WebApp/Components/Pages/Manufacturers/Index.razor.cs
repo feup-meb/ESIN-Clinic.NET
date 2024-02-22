0b615ced750c647184e970b3a6ae5e9298614bee
Leandro Gabriel <leandro@bigiogabriel.com>
2024-02-22 15:48:07 +0000

    Add manufacturer grid

--- ESIN.Clinic.WebApp/Components/Pages/Manufacturers/Index.razor.cs
+++ ESIN.Clinic.WebApp/Components/Pages/Manufacturers/Index.razor.cs
@@ -9,24 +9,24 @@ public partial class Index
     private IQueryable<Manufacturer>? items;
     // private PaginationState pagination = new() { ItemsPerPage = 10 };
     private string nameFilter = string.Empty;
-
+    
     // private GridSort<Manufacturer> sort = GridSort<Manufacturer>
     //     .ByDescending(x => x.Name);
-
+    
     private IQueryable<Manufacturer>? FilteredItems =>
         items?.Where(x => x.Name.Contains(nameFilter, StringComparison.CurrentCultureIgnoreCase));
-
+    
     protected override async Task OnInitializedAsync()
     {
         items = (await getManufacturersQuery.GetManufacturersAsync()).AsQueryable();
     }
-
+    
     private void HandleCountryFilter(ChangeEventArgs args)
     {
         if (args.Value is string value)
             nameFilter = value;
     }
-
+    
     private void HandleClear()
     {
         if (string.IsNullOrWhiteSpace(nameFilter))
