d330c707c445a56e1df65c02bda5b9a858e8d91d
Leandro Gabriel <leandro@bigiogabriel.com>
2024-02-24 00:34:19 +0000

    Remove controllers

--- ESIN.Clinic.Api/Program.cs
+++ ESIN.Clinic.Api/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,7 +22,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 // app.UseAuthorization();
-app.MapControllers();
 
 var summaries = new[]
 {
