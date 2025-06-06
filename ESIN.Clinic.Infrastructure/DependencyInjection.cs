f2cdd1f78cb29f5295010a65e62deedc1e41c69d
Leandro Gabriel <leandro@bigiogabriel.com>
2025-06-06 16:28:17 +0100

    Remove old connectionString for SQL Server

--- ESIN.Clinic.Infrastructure/DependencyInjection.cs
+++ ESIN.Clinic.Infrastructure/DependencyInjection.cs
@@ -12,7 +12,6 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Default");
-        // services.AddDbContext<ClinicDbContext>(opt => opt.UseSqlServer(connectionString));
         services.AddDbContext<ClinicDbContext>(opt => opt.UseNpgsql(connectionString));
         
         services.AddHostedService<MigratorHostedService>();
